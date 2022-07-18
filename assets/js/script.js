27f760e27b820086b88817e1e267db202f9ee3ab
kibbles232 <97996636+kibbles232@users.noreply.github.com>
2022-07-18 00:12:31 -0400

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+        function btcAddress() {
+      
+            navigator.clipboard.writeText
+                ("1LdG8Frnm5tQQPwe1BACDNtL2Ffoq5saCh");
+        }
+	function ethAddress() {
+      
+            navigator.clipboard.writeText
+                ("0x0aAeaa7409a50157098BffAAa43B16a6572adC6F");
+        }
+	function xmrAddress() {
+      
+            navigator.clipboard.writeText
+                ("47ZMkmFNr76BQSL2GGpDjFCA5EPqBGKXmWUvR6tpfqVsNqcBby9WDNreHVgfmrTc9RauoSn3LfbFzXuChpwv3qDm4UxKoWb");
+        }
+	function ltcAddress() {
+      
+            navigator.clipboard.writeText
+                ("LZa7RcVAxhdR6NciVfug3XLLYabYQJ13rF");
+        }
